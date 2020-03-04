d041aa31a61831246b47e88c487d5643cd565f85
Dicky Sanjaya <dicky0201@gmail.com>
2020-03-04 12:40:42 +0700

    formating

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -54,10 +54,11 @@ if __name__ == '__main__':
 
         diff = cv2.absdiff(frame1, frame2)
         gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5,5), 0)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
         _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
         dilated = cv2.dilate(thresh, None, iterations=3)
-        contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(
+            dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
             (x, y, w, h) = cv2.boundingRect(contour)
